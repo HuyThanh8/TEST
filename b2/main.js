6c5a20fce611594cc6e86de1025d0f9ba5e4e922
HuyThanh <pht2581995@gmail.com>
2023-02-14 14:16:18 +0700

    test

--- b2/main.js
+++ b2/main.js
@@ -1,13 +1,13 @@
 function numberOneTriangle() {
-    const n = Number(prompt("Nhập n", 1))
+  const n = Number(prompt("Nhập n", 1))
 
-    for (let i = 0; i < n; i++) {
-      
-      for (let j = 0; j <= i; j++) {
-        document.write("*");
-      }
-      document.write("<br/>")
+  for (let i = 0; i < n; i++) {
+
+    for (let j = 0; j <= i; j++) {
+      document.write("*");
     }
-    
+    document.write("<br/>")
   }
-  numberOneTriangle()
+
+}
+numberOneTriangle()
